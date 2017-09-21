c7d0ce3e7d62ac2688ce3a02748b89350e881f66
Nikita Mikhaliuk <nikkita1992@mail.ru>
2017-09-21 19:28:27 +0300

    12 of 25 tests passing, main functional is working

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,56 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.states = config.states;
+        this.initial = config.initial;
+        this.CurrentState = config.states[this.initial];
+        let states_keys = Object.keys(this.states);
+        for (let state of states_keys) {
+            this.states[state].toString = () => state;
+        }
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return `${this.CurrentState}`;
+
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (this.states.hasOwnProperty(state)) {
+            this.CurrentState = this.states[state];
+        } else {
+            throw new Error("State is missing");
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (this.CurrentState.transitions.hasOwnProperty(event)) {
+            this.changeState(this.CurrentState.transitions[event]);
+        } else {
+            throw new Error("Event is unavaible from this state");
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.CurrentState = this.states[this.initial]
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,7 +60,19 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let states = Object.keys(this.states);
+        if (event === undefined) {
+            return states;
+        }
+        let StatesOfEvent = [];
+        for (let state of states) {
+            if (this.states[state].transitions.hasOwnProperty(event)) {
+                StatesOfEvent.push(state)
+            }
+        }
+        return StatesOfEvent;
+    }
 
     /**
      * Goes back to previous state.
